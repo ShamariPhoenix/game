940fbf749b2f82aeaa57396d030bb255bbe365aa
adam2401 <adam.aleksa@gmail.com>
2026-02-17 17:20:06 -0500

    added auto format on save

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,43 +4,35 @@ using UnityEditor;
 using JetBrains.Annotations;
 
 
-public class PlayerMovement: MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 
 {
-    public float Max_X=10f;
-    public float Min_X=-10f;
+    public float Max_X = 10f;
+    public float Min_X = -10f;
 
-    public float speed=1;
+    public float speed = 1;
 
     private Vector3 Position;
 
 
     void Update()
     {
-        if (transform.position.x<Max_X && transform.position.x>Min_X)
+        if (transform.position.x < Max_X && transform.position.x > Min_X)
         {
-
-        Movement();
-
+            Movement();
         }
-
-
-      
     }
-    
+
     public void Movement()
     {
-
-        if(Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position+=new Vector3(speed*Time.deltaTime,0,0);
-
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
         }
 
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position-=new Vector3(speed*Time.deltaTime,0,0);
+            transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
         }
-
-   }
+    }
 }
